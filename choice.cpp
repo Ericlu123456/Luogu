d8a1b63038501977eb484b7f5dca07e02582e4f1
Ericlu123456 <125123287+Ericlu123456@users.noreply.github.com>
2025-07-09 09:13:20 +0800

    Create choice.cpp

---
+++ choice.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define A 'A'
+#define B 'B'
+#define C 'C'
+#define D 'D'
+#define Y 'A'
+#define N 'B'
+char ans[]={
+	A,A,A,A,A,
+	B,B,B,B,B,
+};
+int main(){
+	int d; cin>>d;
+	cout<<ans[d-1];
+	return 0;
+} 
